534e600c393fe0eba91a7f5ae87d9caacc6103ff
kodeezh <kodeezh@gmail.com>
2025-05-31 20:18:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById("certForm").onsubmit = function(e) {
   for (const [key, value] of form.entries()) {
     params.append(key, value);
   }
-  const certURL = `${window.location.origin}/birth-certificate/cert.html?${params.toString()}`;
+  const certURL = `${window.location.origin}/BIRTH_CERTIFICATE/cert.html?${params.toString()}`;
   document.getElementById("qrcode").innerHTML = "";
   new QRCode(document.getElementById("qrcode"), certURL);
 };
