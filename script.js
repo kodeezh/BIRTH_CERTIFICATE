6718de1c40591241b54bfbfdb1ba5de01d250f64
kodeezh <kodeezh@gmail.com>
2025-05-31 19:43:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("certForm").onsubmit = function(e) {
+  e.preventDefault();
+  const form = new FormData(e.target);
+  const params = new URLSearchParams();
+  for (const [key, value] of form.entries()) {
+    params.append(key, value);
+  }
+  const certURL = `${window.location.origin}/birth-certificate/cert.html?${params.toString()}`;
+  document.getElementById("qrcode").innerHTML = "";
+  new QRCode(document.getElementById("qrcode"), certURL);
+};
